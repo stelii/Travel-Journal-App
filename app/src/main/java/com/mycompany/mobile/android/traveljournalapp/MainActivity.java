17deb6744bf34f90ba750ab36f6ea1ad97e53ab2
Stelian <fedorca.stelian@home.com>
2020-06-07 22:52:42 +0300

    Added menu and header for nav view

--- app/src/main/java/com/mycompany/mobile/android/traveljournalapp/MainActivity.java
+++ app/src/main/java/com/mycompany/mobile/android/traveljournalapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mycompany.mobile.android.traveljournalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+
     }
+
+
 }
