3a07d8783a6aef38f48a5c1108d176f0d5999849
Stelian <fedorca.stelian@home.com>
2020-06-08 00:16:54 +0300

    Added a toggle button for opening the navigation view

--- app/src/main/java/com/mycompany/mobile/android/traveljournalapp/MainActivity.java
+++ app/src/main/java/com/mycompany/mobile/android/traveljournalapp/MainActivity.java
@@ -1,17 +1,25 @@
 package com.mycompany.mobile.android.traveljournalapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ToggleButton;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView ;
 
         @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +42,34 @@ public class MainActivity extends AppCompatActivity {
                 switch (position){
                     case 0 : tab.setText("Home");
                     break ;
-                    case 1 : tab.setText("Favorites");
+                    case 1 : tab.setText("Favorite trips");
                     break ;
                 }
             }
         }).attach();
 
+            navigationView = findViewById(R.id.navigation_view);
+            drawerLayout = findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggleButton = new ActionBarDrawerToggle(this,drawerLayout,
+                    toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+            drawerLayout.addDrawerListener(toggleButton);
+            toggleButton.syncState();
+
+
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu,menu);
+        return true ;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(navigationView)) drawerLayout.closeDrawer(navigationView);
+        else super.onBackPressed();
+    }
 }
