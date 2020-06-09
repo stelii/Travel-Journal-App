eb84fa2c55db47daa4127667236616c2f5bdf27f
Stelian <fedorca.stelian@home.com>
2020-06-09 12:49:05 +0300

    added functionality for deleting all the trips from the list

--- app/src/main/java/com/mycompany/mobile/android/traveljournalapp/MainActivity.java
+++ app/src/main/java/com/mycompany/mobile/android/traveljournalapp/MainActivity.java
@@ -9,39 +9,45 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.ToggleButton;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.mycompany.mobile.android.traveljournalapp.database.Trip;
+import com.mycompany.mobile.android.traveljournalapp.database.TripViewModel;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
+    private TripViewModel viewModel ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
-
-        launchHomeFragment();
-
         navigationView = findViewById(R.id.navigation_view);
         drawerLayout = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggleButton = new ActionBarDrawerToggle(this, drawerLayout,
-                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
-        drawerLayout.addDrawerListener(toggleButton);
-        toggleButton.syncState();
+        viewModel =
+                new ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory
+                        .getInstance(getApplication())).get(TripViewModel.class);
+
+        launchHomeFragment();
+        setUpToolBar();
 
 
     }
@@ -56,12 +62,34 @@ public class MainActivity extends AppCompatActivity {
         transaction.commit();
     }
 
+    private void setUpToolBar(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggleButton = new ActionBarDrawerToggle(this, drawerLayout,
+                toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(toggleButton);
+        toggleButton.syncState();
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.toolbar_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.delete_all_btn :
+                viewModel.deleteAll();
+                return true;
+            default : return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
