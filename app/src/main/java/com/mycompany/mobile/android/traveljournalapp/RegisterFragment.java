4cfb2b4bdafed4715a55e831b6cdab123ca1974a
Stelian <fedorca.stelian@home.com>
2020-06-23 20:39:14 +0300

    when entering the app for the first time, the user is prompted to register in app

--- app/src/main/java/com/mycompany/mobile/android/traveljournalapp/RegisterFragment.java
+++ app/src/main/java/com/mycompany/mobile/android/traveljournalapp/RegisterFragment.java
@@ -1,18 +1,33 @@
 package com.mycompany.mobile.android.traveljournalapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class RegisterFragment extends Fragment {
+    private EditText nameInput, emailInput, passwordInput ;
+    private Button registerButton ;
 
     public RegisterFragment() {
         // Required empty public constructor
@@ -24,5 +39,60 @@ public class RegisterFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        nameInput = view.findViewById(R.id.register_page_name_input);
+        emailInput = view.findViewById(R.id.register_page_email_input);
+        passwordInput = view.findViewById(R.id.register_page_password_input);
+        registerButton = view.findViewById(R.id.register_page_regbutton);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                registerTheUser();
+            }
+        });
+    }
+
+    private void registerTheUser(){
+        String name = nameInput.getText().toString().trim();
+        String email = emailInput.getText().toString().trim();
+        String password = passwordInput.getText().toString();
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            FirebaseUser newUser = firebaseAuth.getCurrentUser();
+                            UserProfileChangeRequest changeRequest =
+                                    new UserProfileChangeRequest.Builder()
+                                            .setDisplayName(name).build();
+                            newUser.updateProfile(changeRequest)
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                launchMainActivity();
+                                            }
+                                        }
+                                    });
+                        }else{
+                            Toast.makeText(getContext(), "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void launchMainActivity(){
+        Intent intent = new Intent(getActivity(),MainActivity.class);
+        startActivity(intent);
+        getActivity().finish();
     }
 }
--- app/src/main/java/com/mycompany/mobile/android/traveljournalapp/WelcomeActivity.java
+++ app/src/main/java/com/mycompany/mobile/android/traveljournalapp/WelcomeActivity.java
@@ -5,6 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -13,13 +19,34 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        final ImageView appLogo = findViewById(R.id.welcome_activity_app_logo);
+        final TextView appName = findViewById(R.id.welcome_activity_app_name);
+
+        final FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                if(firebaseAuth.getCurrentUser() == null){
+                    appLogo.setVisibility(View.GONE);
+                    appName.setVisibility(View.GONE);
+                    launchRegisterFragment();
+
+                }else{
+                    Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         },1200);
+    }
+
+    private void launchRegisterFragment(){
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_welcome_frame_layout,new RegisterFragment())
+                .commit();
     }
 }
